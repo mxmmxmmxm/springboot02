9df3c52dfc119f3edf88f450d8e89fa26b662fee
马小明 <1552861487@qq.com>
2020-07-05 11:05:45 +0800

    springboot整合mybatis的注解版

--- src/main/java/com/atguigu/MainApplication.java
+++ src/main/java/com/atguigu/MainApplication.java
@@ -3,7 +3,7 @@ package com.atguigu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@MapperScan("com.atguigu.dao")  //生成dao代理
+@MapperScan("com.atguigu.dao")  //生成dao代理
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
--- src/main/java/com/atguigu/dao/UserDao.java
+++ src/main/java/com/atguigu/dao/UserDao.java
@@ -2,10 +2,12 @@ package com.atguigu.dao;
 
 import com.atguigu.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-@Mapper
+//@Mapper
 public interface UserDao {
+    @Select("select * from user")
     List<User> findAll();
 
 }
